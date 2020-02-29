646ced51386170b9507685b4fd95c609b1f0f110
David Decker <ddecker30@gatech.edu>
2020-02-28 22:53:47 -0600

    added python command

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,18 +1,18 @@
 source activate CS7641_A2
 
 # neural network scripts
-./neural_network5.py
-./neural_network5_analysis.py
-./neural_network8.py
-./neural_network8_analysis.py
-./neural_network9.py
-./neural_network9_analysis.py
-./neural_network_final.py
-./neural_network_final_analysis.py
+python ./neural_network5.py
+python ./neural_network5_analysis.py
+python ./neural_network8.py
+python ./neural_network8_analysis.py
+python ./neural_network9.py
+python ./neural_network9_analysis.py
+python ./neural_network_final.py
+python ./neural_network_final_analysis.py
 
 # discrete problems
-./discrete_optimization9.py
-./discrete_optimization10.py
-./discrete_optimization_hp_tuning.py
-./discrete_optimization_problem_complexity.py
+python ./discrete_optimization9.py
+python ./discrete_optimization10.py
+python ./discrete_optimization_hp_tuning.py
+python ./discrete_optimization_problem_complexity.py
 
